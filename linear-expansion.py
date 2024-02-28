38e85c5cb6eca6391b78fe299148bd1b56aefc32
ari-feiglin <ari.feiglin@gmail.com>
2024-02-28 22:33:18 +0200

    Added variables

--- linear-expansion.py
+++ linear-expansion.py
@@ -42,59 +42,78 @@ class Atom:
     def __str__(self):
         return f"{self.atype}[{self.val}:{self.priority}]"
 
-def get_priority(x):
-    if x.isdigit():
-        return ExtNum(None)
-    else:
-        priorities = {'+' : 1, '*' : 2, '**' : 3, ';' : 0}
-        return ExtNum(priorities[x])
-
-operations = {
-            '+' : lambda a,b: a + b,
-            '*' : lambda a,b: a * b,
-            '**' : lambda a,b: a ** b
+priorities = {
+            '+' : ExtNum(1),
+            '*' : ExtNum(2),
+            '**' : ExtNum(3),
+            ';' : ExtNum(0),
+            '=': ExtNum(0)
         }
 
-functions = {
-            'sqrt' : lambda a: math.sqrt(a)
+def create_variable(name, value):
+    macros[name.val] = value
+
+operations = {
+            '+' : lambda a,b: a.val + b.val,
+            '*' : lambda a,b: a.val * b.val,
+            '**' : lambda a,b: a.val ** b.val,
+            '=' : create_variable,
+        }
+
+def print_val(a):
+    print(a.val)
+    return Atom(None, None, None, None, None);
+
+macros = {
+            'sqrt' : lambda a: Atom(math.sqrt(a.val), a.priority, 'v', a.val_fun, 'sqrt' + a.str_rep),
+            'let' : lambda a: Atom(a.val, a.priority, 'u', a.val_fun, 'let' + a.str_rep),
+            'print' : print_val,
         }
 
 applications = {
-    ('i','o') : lambda a,b: Atom(a.val, b.priority, 'o', b.val_fun, a.str_rep + b.str_rep),
-    ('o','i') : lambda a,b: Atom(a.val_fun(a.val, b.val), a.priority, 'i', b.val_fun, a.str_rep + b.str_rep),
-    ('o','o') : lambda a,b: Atom(a.val_fun(a.val, b.val), b.priority, 'o', b.val_fun, a.str_rep + b.str_rep),
-    ('(',')') : lambda a,b: Atom(b.val, a.priority, 'i', b.val_fun, a.str_rep + b.str_rep),
-    ('i',')') : lambda a,b: Atom(a.val, b.priority, ')', a.val_fun, a.str_rep + b.str_rep),
-    ('o',')') : lambda a,b: Atom(a.val_fun(a.val, b.val), a.priority, ')', b.val_fun, a.str_rep + b.str_rep),
-    ('f','i') : lambda a,b: Atom(a.val_fun(b.val), b.priority, 'i', b.val_fun, a.str_rep + b.str_rep)
+    ('v','o') : lambda a,b: Atom(a.val, b.priority, 'o', b.val_fun, a.str_rep + b.str_rep),
+    ('o','v') : lambda a,b: Atom(a.val_fun(a, b), b.priority, 'v', b.val_fun, a.str_rep + b.str_rep),
+    ('o','o') : lambda a,b: Atom(a.val_fun(a, b), b.priority, 'o', b.val_fun, a.str_rep + b.str_rep),
+    ('(',')') : lambda a,b: Atom(b.val, a.priority, 'v', b.val_fun, a.str_rep + b.str_rep),
+    ('v',')') : lambda a,b: Atom(a.val, b.priority, ')', a.val_fun, a.str_rep + b.str_rep),
+    ('o',')') : lambda a,b: Atom(a.val_fun(a, b), a.priority, ')', b.val_fun, a.str_rep + b.str_rep),
+    ('s','v') : lambda a,b: macros[a.val](b),
+    ('s','o') : lambda a,b: Atom(macros[a.val].val, b.priority, 'o', b.val_fun, a.str_rep + b.str_rep),
+    ('s',')') : lambda a,b: Atom(macros[a.val].val, b.priority, ')', a.val_fun, a.str_rep + b.str_rep),
+    ('s','s') : lambda a,b: macros[a.val](b),
+    ('o','s') : lambda a,b: Atom(a.val_fun(a, macros[b.val]), b.priority, 'v', b.val_fun, a.str_rep + b.str_rep),
+    ('u','o') : lambda a,b: Atom(a.val, b.priority, 'o', b.val_fun, a.str_rep + b.str_rep),
+    ('v',';') : lambda a,b: Atom(a.val, b.priority, 'v', a.val_fun, a.str_rep + b.str_rep),
+    ('s',';') : lambda a,b: Atom(a.val, b.priority, 's', a.val_fun, a.str_rep + b.str_rep),
 }
 
 atom_types = {
-            'i' : lambda i: Atom(float(i), ExtNum(0,1), 'i', lambda a,b: Exception("integer type cannot compute value")),
+            'v' : lambda i: Atom(float(i), ExtNum(0,1), 'v', lambda a,b: Exception("integer type cannot compute value")),
             '(' : lambda i: Atom('(', ExtNum(0,1), '(', lambda a,b: Exception("parentheses type cannot compute value")),
             ')' : lambda i: Atom(')', ExtNum(0,0), ')', lambda a,b: Exception("parentheses type cannot compute value")),
-            'o' : lambda o: Atom(o, get_priority(o), 'o', operations[o]),
-            'f' : lambda f: Atom(f, ExtNum(0,1), 'f', functions[f])
+            'o' : lambda o: Atom(o, priorities[o], 'o', operations[o]),
+            's' : lambda s: Atom(s, ExtNum(0,1), 's', lambda: Exception("string type cannot compute value")),
+            ';' : lambda s: Atom(';', ExtNum(0), ';', lambda: Exception("end type cannot compute value")),
         }
 
 independent_atypes = ['(', ')']
 
 atypes = {
             ' ' : None,
-            ';' : None,
+            ';' : [';'],
             '(' : ['('],
             ')' : [')'],
-            '.' : ['o', 'i']
+            '.' : ['o', 'v']
         }
 
 for c in ['~', '!', '@', '#', '$', '%', '^', '&', '*', '-', '+', '=', '/', '|', '<', '>']:
     atypes[c] = ['o']
 
 for i in range(10):
-    atypes[str(i)] = ['i', 'f']
+    atypes[str(i)] = ['v', 's']
 
 for c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
-    atypes[c] = ['f']
+    atypes[c] = ['s']
 
 def add_atom(atoms, tok, atype):
     if atype == None: return
@@ -103,7 +122,7 @@ def add_atom(atoms, tok, atype):
 def convert_to_atoms(s):
     curr_tok = ''
     curr_atype = None
-    atoms = [atom_types['('](None)]
+    atoms = []
 
     for c in s:
         if c not in atypes:
@@ -123,8 +142,9 @@ def convert_to_atoms(s):
             curr_tok = c
         else:
             curr_tok += c
+    add_atom(atoms, curr_tok, curr_atype)
 
-    return atoms + [atom_types[')'](None)]
+    return atoms + []
 
 def print_atoms_list(atoms):
     for atom in atoms:
@@ -134,22 +154,21 @@ def print_atoms_list(atoms):
 def reduce_atoms(atoms):
     if len(atoms) == 1: return None
     if atoms[0].priority >= atoms[1].priority and (atoms[0].atype, atoms[1].atype) in applications:
-        return [applications[(atoms[0].atype,atoms[1].atype)](atoms[0], atoms[1])] + atoms[2:]
+        new_atom = applications[(atoms[0].atype,atoms[1].atype)](atoms[0], atoms[1])
+        return [new_atom] + atoms[2:] if new_atom.val != None else atoms[2:]
     else:
         return [atoms[0]] + reduce_atoms(atoms[1:])
 
 def full_reduce(atoms):
     last_atoms = atoms
     while atoms != None:
-        print_atoms_list(atoms)
+        #print_atoms_list(atoms)
         last_atoms = atoms
         atoms = reduce_atoms(atoms)
     return last_atoms[0].val
 
-test = "sqrt((2+3)) + 3.2;"
+expression = input(">> ")
 
-print(test)
-atoms = convert_to_atoms(test)
-print_atoms_list(atoms)
-print(full_reduce(atoms))
+atoms = convert_to_atoms(expression)
+full_reduce(atoms)
 
